9fd29e4a6267f4538babf9319230e40643ddd962
Bralabee <Bralabee@users.noreply.github.com>
2025-12-19 10:13:55 +0000

    fix: correct __init__.py docstring syntax error

--- src/usf_fabric_monitoring/__init__.py
+++ src/usf_fabric_monitoring/__init__.py
@@ -1,5 +1,3 @@
-"""
-"""USF Fabric Monitoring - Microsoft Fabric Monitoring & Governance System
-"""
+"""USF Fabric Monitoring - Microsoft Fabric Monitoring & Governance System."""
 
 __version__ = "0.3.11"
